a5a13aab82343595c7157380b071c8dc7a619ae4
Mukul <mukulkashyap.business@gmail.com>
2026-09-08 12:49:48 +0530

    add worker function for fetching URLs

---
+++ cmd/crawler/worker.go
@@ -0,0 +1,43 @@
+package crawler
+
+import (
+	"fmt"
+	"log"
+	"sync"
+)
+
+func fetchUrl(url string) ([]string, error) {
+	htmlBody, contentType, statusCode, err := GetPageData(url)
+	if err != nil {
+		fmt.Printf("Error while fetching the page data for %s: %v\n", url, err)
+		return nil, err
+	}
+	fmt.Printf("Content Type: %s\n", contentType)
+	fmt.Printf("Status Code: %d\n", statusCode)
+
+	links, err := GetHTMLData(htmlBody)
+	if err != nil {
+		return nil, fmt.Errorf("Problem while parsing the HTML: %w", err)
+	}
+	return links, err
+
+}
+
+func worker(f *Frontier, wg *sync.WaitGroup, v *VisitedSet) {
+	for url := range f.out {
+		links, err := fetchUrl(url)
+		if err != nil {
+			log.Fatalf("Problem with fetching or parsing the URL: %v", err)
+		}
+
+		for _, link := range links {
+			if !v.IsVisited(link) { // check for already visited URL
+				wg.Add(1)
+				f.push(link)
+			}
+		}
+		fmt.Printf("Visited URL: %s", url)
+		wg.Done() // URL parsed
+	}
+
+}
