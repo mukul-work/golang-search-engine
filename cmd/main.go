bbb83e3de2f08c8c5923902c296e623495c40794
Mukul <mukulkashyap.business@gmail.com>
2026-09-08 13:02:21 +0530

    link everything together in the main.go file

--- cmd/main.go
+++ cmd/main.go
@@ -7,40 +7,51 @@ import (
 	"github.com/mukul-work/golang-web-crawler/cmd/crawler"
 )
 
-type Result struct {
-	URL   string `json:"url"`
-	Title string `json:"title"`
-}
+// type Urls struct {
+// 	URL   string `json:"url"`
+// }
 
-func fetchUrl(url string, wg *sync.WaitGroup) {
-	defer wg.Done()
-	htmlBody, contentType, statusCode, err := crawler.GetPageData(url)
-	if err != nil {
-		fmt.Printf("Error while fetching the page data for %s: %v\n", url, err)
-		return
-	}
-	fmt.Printf("Content Type: %s\n", contentType)
-	fmt.Printf("Status Code: %d\n", statusCode)
+// func fetchUrl(url string, wg *sync.WaitGroup) {
+// 	defer wg.Done()
+// 	htmlBody, contentType, statusCode, err := crawler.GetPageData(url)
+// 	if err != nil {
+// 		fmt.Printf("Error while fetching the page data for %s: %v\n", url, err)
+// 		return
+// 	}
+// 	fmt.Printf("Content Type: %s\n", contentType)
+// 	fmt.Printf("Status Code: %d\n", statusCode)
 
-	links, err := crawler.GetHTMLData(htmlBody)
-	fmt.Printf("Links for URL: %s", url)
-	for _, link := range links {
-		fmt.Printf("Link: %s\n", link)
-	}
+// 	links, err := crawler.GetHTMLData(htmlBody)
+// 	fmt.Printf("Links for URL: %s", url)
+// 	for _, link := range links {
+// 		fmt.Printf("Link: %s\n", link)
+// 	}
 
-}
+// }
 
 func main() {
-	urls := []string{"https://en.wikipedia.org/wiki/Computer", "https://dev.to/jones_charles_ad50858dbc0/building-a-high-concurrency-web-crawler-in-go-a-practical-guide-i3a"}
-	ch := make(chan Result, len(urls))
+
+	f := crawler.NewFrontier()
+	visited := crawler.NewVisitedSet()
 	var wg sync.WaitGroup
 
-	for _, url := range urls {
-		wg.Add(1)
-		go fetchUrl(url, &wg)
+	numWorkers := 10
+	for i := 0; i < numWorkers; i++ {
+		go crawler.Worker(f, &wg, visited)
+	}
+	seeds := []string{
+		"https://example.com",
+		"https://example2.com",
 	}
 
+	for _, seed := range seeds {
+		if !visited.IsVisited(seed) {
+			wg.Add(1)
+			f.Push(seed)
+		}
+	}
 	wg.Wait()
-	close(ch)
+	f.Close()
+	fmt.Println("Crawl complete. Total URLs visited:", visited.Count())
 
 }
